1dff6a701dedf18f2b92165fadd6062350f8531d
WM <wmishima@gmail.com>
2022-04-17 22:12:26 +0900

    3-2-3

--- album/urls.py
+++ album/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from django.conf import settings 
 from django.conf.urls.static import static 
  
-from . import views 
+from .views import PhotoListView 
  
 urlpatterns = [ 
-    path("", views.index), 
+    path("", PhotoListView.as_view(), name="photo-list"), 
 ] 
  
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
